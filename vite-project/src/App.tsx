c74616a0e5dee5c05889e30c748616dd30f5c328
lissCez <lissCez@gmail.com>
2024-02-23 12:09:39 -0300

    enviando mais um commit

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -8,7 +8,22 @@ function App() {
 
   return (
     <>
-      <h1>oi ;b</h1>
+        <div>
+            <a href='/react.svg' target='_blank'><img src={reactLogo} ></img></a>
+            <a href='/vite.svg' target='_blank'><img src={viteLogo} ></img></a>
+            <h1>Vite + React</h1>
+            <div className='card'>
+                <button onClick={() => setCount((contagem) => contagem + 1)}>
+                    A contagem de clicks está em: {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code>
+                </p>
+            </div>
+            <p className='read-the-docs'>
+                Click on the Vite and React logos to learn more
+            </p>
+        </div>
     </>
   )
 }
